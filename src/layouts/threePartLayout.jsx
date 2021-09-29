4e8debdfd1faadd702de3ef764a7fe2cb1dabe65
cyfinfaza <42043351+cyfinfaza@users.noreply.github.com>
2021-09-29 17:40:57 -0400

    repair broken grid layout

--- src/layouts/threePartLayout.jsx
+++ src/layouts/threePartLayout.jsx
@@ -31,7 +31,21 @@ export default function ThreePartLayout(props) {
 					<p>This is some lesson content.</p>
 					<h2>Want some placeholder?</h2>
 					<p>Here you go</p>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum
+						dolor sit amet, consectetur adipisicing elit. Ipsum necessitatibus
+						neque omnis similique? Voluptates expedita laborum vero culpa
+						dolores eaque beatae quibusdam, esse cupiditate consequatur nam,
+						provident ea nihil fugit. Vitae aut praesentium tempora consectetur
+						optio! Delectus eveniet quas provident, facere reprehenderit ipsam
+						sapiente aliquid? Vitae odio rerum, atque reprehenderit, dignissimos
+						recusandae nihil harum ab quia ad ea quam accusantium magni
+						inventore iste quidem architecto dolor rem. Ipsam, adipisci beatae
+						nostrum nam fugiat similique ad non reprehenderit molestias quas
+						recusandae ea eveniet harum mollitia dolor maxime voluptates
+						explicabo doloremque dolore. Aut voluptatem laboriosam soluta
+						itaque, similique ab officia architecto illo.
+					</p>
 				</div>
 				<div className={layoutStyle.editor}>
 					<Editor
