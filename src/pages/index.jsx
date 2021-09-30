94c37f591e3a1012e7a09395d3a9e5926deaaf8e
cyfinfaza <42043351+cyfinfaza@users.noreply.github.com>
2021-09-29 22:10:41 -0400

    modify placeholder index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,10 +3,11 @@ import * as React from "react";
 const isBrowser = typeof window !== "undefined";
 
 const IndexPage = () => {
-	isBrowser && (window.location = "/course");
+	// isBrowser && (window.location = "/course");
 	return (
-		<p>
-			Visit <a href="/course">course page</a>
+		<p style={{ margin: "16px" }}>
+			This site is currently in development. Visit the{" "}
+			<a href="/course">course page</a> for a sneak peak at what's coming.
 		</p>
 	);
 };
